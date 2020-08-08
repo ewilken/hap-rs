597b433ebc9f59088e529f0a3589b87d6936e018
Robert Schütte <schuetterobert@gmail.com>
2020-08-08 17:22:14 +0200

    Fix windows support

--- src/storage/file_storage.rs
+++ src/storage/file_storage.rs
@@ -3,7 +3,6 @@ use std::{
     ffi::OsStr,
     fs,
     io::{self, BufReader, BufWriter, ErrorKind, Read, Write},
-    os::unix::fs::PermissionsExt,
     path::{Path, PathBuf},
     str,
 };
@@ -27,9 +26,17 @@ impl FileStorage {
         let dir_path = spawn_blocking(move || -> Result<PathBuf> {
             fs::create_dir_all(&dir_path)?;
 
-            let mut perms = fs::metadata(&dir_path)?.permissions();
-            perms.set_mode(0o777);
-            fs::set_permissions(&dir_path, perms)?;
+            // The file permission setting is only avialble for unix
+            #[cfg(all(unix))]
+            {
+                // only include permission trait on unix
+                use std::os::unix::fs::PermissionsExt;
+
+                // set file permission for unix
+                let mut perms = fs::metadata(&dir_path)?.permissions();
+                perms.set_mode(0o777);
+                fs::set_permissions(&dir_path, perms)?;
+            }
 
             Ok(dir_path)
         })
