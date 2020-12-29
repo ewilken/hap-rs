9c2b3fb63a5a1058604efea8084460cec9e7a4e2
Gaelan Steele <gbs@canishe.com>
2020-12-28 23:10:59 -0800

    Fix bug in FileStorage.
    FileStorage was opening files for writing without turning on truncate,
    so if the new contents were smaller, we'd leave some of the old file
    behind, making it invalid JSON.

--- src/transport/bonjour.rs
+++ src/transport/bonjour.rs
@@ -1,14 +1,14 @@
 use serde::{Deserialize, Serialize};
 
 /// Bonjour Feature Flag.
-#[derive(Debug, Copy, Clone, Serialize, Deserialize)]
+#[derive(Debug, Copy, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub enum BonjourFeatureFlag {
     Zero = 0,
     MfiCompliant = 1,
 }
 
 /// Bonjour Status Flag.
-#[derive(Debug, Copy, Clone, Serialize, Deserialize)]
+#[derive(Debug, Copy, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub enum BonjourStatusFlag {
     Zero = 0,
     NotPaired = 1,
