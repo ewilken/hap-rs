015120e746e241f547647d408c51b7af062ff6aa
Elias Wilken <elias@wlkn.io>
2021-11-09 00:07:49 +0100

    extend `Characteristic<T>` & `HapCharacteristic`
    New methods on the `HapCharacteristic` trait:
    ```rust
    fn set_id(&mut self, id: u64);
    fn set_type(&mut self, hap_type: HapType);
    fn set_format(&mut self, format: Format);
    fn set_perms(&mut self, perms: Vec<Perm>);
    fn get_description(&self) -> Option<String>;
    fn set_description(&mut self, description: Option<String>);
    fn set_unit(&mut self, unit: Option<Unit>);
    fn set_max_value(&mut self, max_value: Option<serde_json::Value>);
    fn set_min_value(&mut self, min_value: Option<serde_json::Value>);
    fn set_step_value(&mut self, step_value: Option<serde_json::Value>);
    fn set_max_len(&mut self, max_len: Option<u16>);
    fn get_max_data_len(&self) -> Option<u32>;
    fn set_max_data_len(&mut self, max_data_len: Option<u32>);
    fn get_valid_values(&self) -> Option<Vec<serde_json::Value>>;
    fn set_valid_values(&mut self, valid_values: Option<Vec<serde_json::Value>>) -> Result<()>;
    fn get_valid_values_range(&self) -> Option<[serde_json::Value; 2]>;
    fn set_valid_values_range(&mut self, valid_values_range: Option<[serde_json::Value; 2]>) -> Result<()>;
    fn get_ttl(&self) -> Option<u64>;
    fn set_ttl(&mut self, ttl: Option<u64>);
    fn get_pid(&self) -> Option<u64>;
    fn set_pid(&mut self, pid: Option<u64>);
    ```
    
    New methods on the `Characteristic<T>` struct:
    ```rust
    pub fn new(id: u64, accessory_id: u64, hap_type: HapType, format: Format, perms: Vec<Perm>, description: Option<String>, event_notifications: Option<bool>, value: T, unit: Option<Unit>, max_value: Option<T>, min_value: Option<T>, step_value: Option<T>, max_len: Option<u16>, max_data_len: Option<u32>, valid_values: Option<Vec<T>>, valid_values_range: Option<[T; 2]>, ttl: Option<u64>, pid: Option<u64>) -> Self;
    pub fn set_id(&mut self, id: u64);
    pub fn set_type(&mut self, hap_type: HapType);
    pub fn set_format(&mut self, format: Format);
    pub fn set_perms(&mut self, perms: Vec<Perm>);
    pub fn get_description(&self) -> Option<String>;
    pub fn set_unit(&mut self, unit: Option<Unit>);
    pub fn set_max_len(&mut self, val: Option<u16>);
    pub fn get_max_data_len(&self) -> Option<u32>;
    pub fn set_max_data_len(&mut self, val: Option<u32>);
    pub fn get_valid_values(&self) -> Option<Vec<T>>;
    pub fn set_valid_values(&mut self, val: Option<Vec<T>>);
    pub fn get_valid_values_range(&self) -> Option<[T; 2]>;
    pub fn set_valid_values_range(&mut self, val: Option<[T; 2]>);
    pub fn get_ttl(&self) -> Option<u64>;
    pub fn set_ttl(&mut self, val: Option<u64>);
    pub fn get_pid(&self) -> Option<u64>;
    pub fn set_pid(&mut self, val: Option<u64>);
    ```

--- src/characteristic/generated/setup_transfer_transport.rs
+++ src/characteristic/generated/setup_transfer_transport.rs
@@ -62,11 +62,23 @@ impl SetupTransferTransportCharacteristic {
 impl HapCharacteristic for SetupTransferTransportCharacteristic {
     fn get_id(&self) -> u64 { self.0.get_id() }
 
+    fn set_id(&mut self, id: u64) { self.0.set_id(id) }
+
     fn get_type(&self) -> HapType { self.0.get_type() }
+
+    fn set_type(&mut self, hap_type: HapType) { self.0.set_type(hap_type) }
 
     fn get_format(&self) -> Format { self.0.get_format() }
 
+    fn set_format(&mut self, format: Format) { self.0.set_format(format) }
+
     fn get_perms(&self) -> Vec<Perm> { self.0.get_perms() }
+
+    fn set_perms(&mut self, perms: Vec<Perm>) { self.0.set_perms(perms) }
+
+    fn get_description(&self) -> Option<String> { self.0.get_description() }
+
+    fn set_description(&mut self, description: Option<String>) { self.0.set_description(description) }
 
     fn get_event_notifications(&self) -> Option<bool> { self.0.get_event_notifications() }
 
@@ -99,13 +111,91 @@ impl HapCharacteristic for SetupTransferTransportCharacteristic {
 
     fn get_unit(&self) -> Option<Unit> { self.0.get_unit() }
 
+    fn set_unit(&mut self, unit: Option<Unit>) { self.0.set_unit(unit) }
+
     fn get_max_value(&self) -> Option<serde_json::Value> { self.0.get_max_value().map(|v| json!(v)) }
+
+    fn set_max_value(&mut self, max_value: Option<serde_json::Value>) -> Result<()> {
+        self.0.set_max_value(match max_value {
+            Some(v) => Some(serde_json::from_value(v).map_err(|_| Error::InvalidValue(self.get_format()))?),
+            None => None,
+        });
+
+        Ok(())
+    }
 
     fn get_min_value(&self) -> Option<serde_json::Value> { self.0.get_min_value().map(|v| json!(v)) }
 
+    fn set_min_value(&mut self, min_value: Option<serde_json::Value>) -> Result<()> {
+        self.0.set_min_value(match min_value {
+            Some(v) => Some(serde_json::from_value(v).map_err(|_| Error::InvalidValue(self.get_format()))?),
+            None => None,
+        });
+
+        Ok(())
+    }
+
     fn get_step_value(&self) -> Option<serde_json::Value> { self.0.get_step_value().map(|v| json!(v)) }
 
+    fn set_step_value(&mut self, step_value: Option<serde_json::Value>) -> Result<()> {
+        self.0.set_step_value(match step_value {
+            Some(v) => Some(serde_json::from_value(v).map_err(|_| Error::InvalidValue(self.get_format()))?),
+            None => None,
+        });
+
+        Ok(())
+    }
+
     fn get_max_len(&self) -> Option<u16> { self.0.get_max_len() }
+
+    fn set_max_len(&mut self, max_len: Option<u16>) { self.0.set_max_len(max_len) }
+
+    fn get_max_data_len(&self) -> Option<u32> { self.0.get_max_data_len() }
+
+    fn set_max_data_len(&mut self, max_data_len: Option<u32>) { self.0.set_max_data_len(max_data_len) }
+
+    fn get_valid_values(&self) -> Option<Vec<serde_json::Value>> {
+        self.0
+            .get_valid_values()
+            .map(|v| v.into_iter().map(|v| json!(v)).collect())
+    }
+
+    fn set_valid_values(&mut self, valid_values: Option<Vec<serde_json::Value>>) -> Result<()> {
+        self.0.set_valid_values(match valid_values {
+            Some(v) => Some(
+                v.into_iter()
+                    .map(|v| serde_json::from_value(v).map_err(|_| Error::InvalidValue(self.get_format())))
+                    .collect::<Result<Vec<Vec<u8>>>>()?,
+            ),
+            None => None,
+        });
+
+        Ok(())
+    }
+
+    fn get_valid_values_range(&self) -> Option<[serde_json::Value; 2]> {
+        self.0.get_valid_values_range().map(|v| [json!(v[0]), json!(v[1])])
+    }
+
+    fn set_valid_values_range(&mut self, valid_values_range: Option<[serde_json::Value; 2]>) -> Result<()> {
+        self.0.set_valid_values_range(match valid_values_range {
+            Some([start, end]) => Some(Result::<[Vec<u8>; 2]>::Ok([
+                serde_json::from_value(start).map_err(|_| Error::InvalidValue(self.get_format()))?,
+                serde_json::from_value(end).map_err(|_| Error::InvalidValue(self.get_format()))?,
+            ])?),
+            None => None,
+        });
+
+        Ok(())
+    }
+
+    fn get_ttl(&self) -> Option<u64> { self.0.get_ttl() }
+
+    fn set_ttl(&mut self, ttl: Option<u64>) { self.0.set_ttl(ttl) }
+
+    fn get_pid(&self) -> Option<u64> { self.0.get_pid() }
+
+    fn set_pid(&mut self, pid: Option<u64>) { self.0.set_pid(pid) }
 }
 
 impl HapCharacteristicSetup for SetupTransferTransportCharacteristic {
